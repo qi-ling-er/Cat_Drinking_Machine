ad031cd5cbdd091b1c95298f326c5e0ba68d34d3
702 <124680954+qi-ling-er@users.noreply.github.com>
2024-04-26 14:37:35 +0800

    Delete HEX directory

--- HEX/core_cm3.d
+++
@@ -1,2 +0,0 @@
-..\hex\core_cm3.o: Start\core_cm3.c
-..\hex\core_cm3.o: E:\soft\STM 32\Keil MDK\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
--- HEX/startup_stm32f10x_md.d
+++
@@ -1,1 +0,0 @@
-..\hex\startup_stm32f10x_md.o: Start\startup_stm32f10x_md.s
